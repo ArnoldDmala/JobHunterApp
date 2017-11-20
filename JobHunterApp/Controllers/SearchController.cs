e867714cb510a93b8a0fdb6e3080ca3e9df60e1e
Brantley Jenkins <bcj@email.uscupstate.edu>
2017-11-20 18:25:13 -0500

    Added search controller+search box

---
+++ JobHunterApp/Controllers/SearchController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobHunterApp.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Search(string SearchString)
+        {
+            ViewBag.SearchString = SearchString;
+            ViewBag.Title = "Search";
+            return View("SearchResult");
+        }
+    }
+}
